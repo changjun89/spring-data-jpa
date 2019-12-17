03013d7fbe59c1718262ea5783bb143973d8b3f0
leechang0423 <leechang0423@naver.com>
2019-12-17 16:41:05 +0900

    Web확장 도메인 클래스 컨버터

---
+++ src/main/java/me/changjun/jpa/controller/MemberController.java
@@ -0,0 +1,33 @@
+package me.changjun.jpa.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.changjun.jpa.entity.Member;
+import me.changjun.jpa.repository.MemberRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Long id) {
+        Member member = memberRepository.findById(id).get();
+        return member.getUsername();
+    }
+
+    @GetMapping("/members2/{id}")
+    public String findMember2(@PathVariable("id") Member member) {
+        return member.getUsername();
+    }
+
+    @PostConstruct
+    public void init() {
+        memberRepository.save(new Member("userA"));
+    }
+}
